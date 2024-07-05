9c05ccedee8a66d284b3fc83f1a2b796e1da9b11
sridhars650 <164981605+sridhars650@users.noreply.github.com>
2024-07-04 17:58:56 -0700

    Centralized the logic in one script; made a path so it says when it's done with tutorial(add on later); reconfigured another level

--- Assets/TutorialLogic.cs
+++ Assets/TutorialLogic.cs
@@ -1,35 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinishedScript : MonoBehaviour
+public class TutorialLogic : MonoBehaviour
 {
     public bool gameIsFrozen = false;
     public bool inMessageState = false;
     public GameObject messageObject;
     private BoxCollider2D boxCollider;
-    public bool finishedTutorial = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         // No operations needed in Update method currently
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 3)
-        {
-            inMessageState = true;
-            FreezeGame(true); // freezes game
-            PopUpMessage();
-            finishedTutorial = true;
-        }
     }
 
     public void FreezeGame(bool freeze)
@@ -38,8 +22,21 @@ public class FinishedScript : MonoBehaviour
         Time.timeScale = freeze ? 0 : 1;
     }
 
-    public void PopUpMessage()
+    public void ResetTutorial()
     {
-        messageObject.SetActive(true);
+        FreezeGame(false); // unfreezes
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+    public void returnToMenu()
+    {
+        FreezeGame(false); // unfreezes
+        SceneManager.LoadScene("TitleScene");
+    }
+
 }
